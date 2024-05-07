4c1c17050afd50bcce1a707922a86639c776edfd
sds3000 <64442811+sds3000@users.noreply.github.com>
2024-05-07 12:20:17 -0400

    initial commit

---
+++ intro_to_programming_with_python/intro_to_programming/data_types_excersise.py
@@ -0,0 +1,15 @@
+names = (
+    "Asta",
+    "Butterschotch",
+    "Pudding",
+    "Neptune",
+    "Darwin",
+)
+
+pets = {
+    "Asta": "dog",
+    "Butterscotch": "cat",
+    "Pudding": "cat",
+    "Neptune": "fish",
+    "Darwin": "lizard",
+}
