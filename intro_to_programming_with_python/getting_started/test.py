baa342ac6b8b076b2e37312fd595be502680dd7b
sds3000 <64442811+sds3000@users.noreply.github.com>
2024-05-08 10:03:12 -0400

    created the Input_Output.py file

--- intro_to_programming_with_python/getting_started/test.py
+++
@@ -1,8 +0,0 @@
-print(
-    len(
-        "how many characters is this. any more and this might be over the limit just a  "
-    )  # the length of 79 chars is this long. no line of code or comment should be longer
-)
-# break up lines
-my_list = ["one", "two", "three"]
-my_tuple = (3.1, 2.1, 1.1)  # can be put on seperate lines without a problem
