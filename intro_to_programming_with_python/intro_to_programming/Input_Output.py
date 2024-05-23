04147d62d9ac9f6009dbb1c445eb5d594d9417a4
sds3000 <64442811+sds3000@users.noreply.github.com>
2024-05-23 09:51:37 -0400

    changes to In[ut_Output.py]

--- intro_to_programming_with_python/intro_to_programming/Input_Output.py
+++ intro_to_programming_with_python/intro_to_programming/Input_Output.py
@@ -4,30 +4,23 @@ your name, then output Hello, NAME! where NAME is the name you
 entered:
 """
 
-# name = input("What is your name? ")
-
-# print(f"Hello, {name}")
+name = input("name? ")
+print(f"Hello, {name}")
 
 """Modify the greeter.py program to ask for the user's first 
 and last names separately, then greet the user with their 
 full name.
 """
-
-# first = input("What is your first name? ")
-# last = input("What is your last name? ")
-
-# print(f"Hello, {first} {last}!")
-
+first_name = input("first name")
+last_name = input("last name? ")
+print(f"hello, {first_name} {last_name}")
 """
 Write a program named age.py that asks the user to enter their 
 age, then calculates and reports the future age 10, 20, 30, and 
 40 years from now.
 """
 
-# age = int(input("How old are you? "))
-# years = 10
+age = int(input("age?"))
 
-# while years <= 40:
-#     print(f"In {years}, you will be {age + years} years old.")
-#     years += 10
-
+for x in range(10, 50, 10):
+    print(f"In {x} years, you will be {age + x} years old")
